4564e7f26b054e6838427695da188002cec51752
Jarvissilva <jarvissilva212@gmail.com>
2025-09-12 11:10:15 +0530

    fake

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -5,11 +5,40 @@ import Facebook from "next-auth/providers/facebook";
 import Twitter from "next-auth/providers/twitter";
 import dbclient from "@/utils/dbclient";
 
-
-
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: MongoDBAdapter(dbclient),
   providers: [Google, Facebook, Twitter({ version: "2.0" })],
   secret: process.env.NEXTAUTH_SECRET,
 
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      if (!user.email?.endsWith("@gmail.com")) {
+        console.log("❌ Sign-in blocked: not a Gmail account");
+        return false;
+      }
+
+      if (!user.role) {
+        user.role = "user";
+      }
+      return true;
+    },
+
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+
+    async session({ session, token, user }) {
+      if (token?.id) {
+        session.user.id = token.id;
+      }
+      return session;
+    },
+
+    async redirect({ url, baseUrl }) {
+      return baseUrl;
+    },
+  },
 });
